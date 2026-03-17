f474ce8346d6a1a22d4944d446c4a66c9b27c6dd
Nour Lababidi <nourzymta@gmail.com>
2026-03-17 02:11:47 +0200

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "pixxicrm.ae", // <-- remove www
-        pathname: "/api/profile/upload/**",
-      },
-      // Optional: allow all paths under this host
-      {
-        protocol: "https",
-        hostname: "pixxicrm.ae",
-        pathname: "/**",
-      },
-    ],
+    domains: ["pixxicrm.ae", "www.pixxicrm.ae"],
   },
 };
 
